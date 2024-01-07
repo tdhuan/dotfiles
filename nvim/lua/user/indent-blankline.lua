c047e290dea13a4f2aee0ddd6f18bb937820ed8b
tdhuan <dinhhuantran3010@gmail.com>
2024-01-07 13:59:38 +0700

    Update indent-blankline configuration

--- nvim/lua/user/indent-blankline.lua
+++ nvim/lua/user/indent-blankline.lua
@@ -1,15 +1,16 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, indent_blankline = pcall(require, "ibl")
 if not status_ok then
 	return
 end
+indent_blankline.setup()
 
-vim.opt.list = true
-vim.opt.listchars:append("eol:↴")
-
-indent_blankline.setup({
-	show_current_context = true,
-	show_current_context_start = true,
-	space_char_blankline_highlight = true,
-})
-vim.cmd([[highlight IndentBlanklineContextStart guifg=#E5C07B gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineSpaceChar guifg=#E5C07B gui=nocombine]])
+-- vim.opt.list = true
+-- vim.opt.listchars:append("eol:↴")
+--
+-- indent_blankline.setup({
+-- 	show_current_context = true,
+-- 	show_current_context_start = true,
+-- 	space_char_blankline_highlight = true,
+-- })
+-- vim.cmd([[highlight IndentBlanklineContextStart guifg=#E5C07B gui=nocombine]])
+-- vim.cmd([[highlight IndentBlanklineSpaceChar guifg=#E5C07B gui=nocombine]])
