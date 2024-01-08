fe5f44cf12e274f7d239fcbbd62149998c64ead7
tdhuan <dinhhuantran3010@gmail.com>
2024-01-08 16:21:28 +0700

    Install codeium for ubuntu

--- ubuntu/nvim/lua/user/plugins.lua
+++ ubuntu/nvim/lua/user/plugins.lua
@@ -129,6 +129,16 @@ return require("packer").startup(function(use)
 	-- easily search for, substitute, and abbreviate multiple variants of a word
 	use({ "tpope/vim-abolish" })
 	use({ "github/copilot.vim" })
+  use ({
+    'Exafunction/codeium.vim',
+    config = function ()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set('i', '<C-l>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    end
+  })
 
 	-- Markdown preview
 	use({ "ellisonleao/glow.nvim", branch = "main" })
