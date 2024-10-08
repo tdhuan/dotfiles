ddd7bd3ae6cc576df726ced20ecee04c0986ac15
tdhuan <dinhhuantran3010@gmail.com>
2024-10-08 11:40:29 +0700

    Disable lazygit config

--- ubuntu/lzvim/lua/config/options.lua
+++ ubuntu/lzvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.lazygit_config = false
 local opt = vim.opt
 opt.relativenumber = false
 opt.wrap = true
