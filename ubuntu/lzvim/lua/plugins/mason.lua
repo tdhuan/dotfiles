adc345e51c42d09ab3916c8ce82d4b8dfce6ad52
tdhuan <dinhhuantran3010@gmail.com>
2024-10-10 15:02:01 +0700

    [ubuntu] - Ensure language servers are installed

---
+++ ubuntu/lzvim/lua/plugins/mason.lua
@@ -0,0 +1,17 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shfmt",
+      "vtsls",
+      "eslint-lsp",
+      "prettierd",
+      "eslint_d",
+      "tailwindcss-language-server",
+      "solargraph",
+      "rubocop",
+      "rescript-language-server",
+    },
+  },
+}
