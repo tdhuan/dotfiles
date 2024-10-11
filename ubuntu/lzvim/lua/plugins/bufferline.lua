a294803d6f75effb34e2c2abc440b3e1bd0ebeee
tdhuan <dinhhuantran3010@gmail.com>
2024-10-11 10:22:51 +0700

    [ubuntu] - update bufferline

--- ubuntu/lzvim/lua/plugins/bufferline.lua
+++ ubuntu/lzvim/lua/plugins/bufferline.lua
@@ -9,8 +9,8 @@ return {
       end,
       close_command = "bdelete! %d",
       right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-      left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
-      middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
+      left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
+      middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
       indicator = {
         icon = "▎",
         style = "icon",
@@ -20,6 +20,12 @@ return {
       close_icon = "",
       left_trunc_marker = "",
       right_trunc_marker = "",
+      name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
+        -- remove extension from markdown files for example
+        if buf.name:match("%.md") then
+          return vim.fn.fnamemodify(buf.name, ":t:r")
+        end
+      end,
       max_name_length = 18,
       max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
       tab_size = 18,
