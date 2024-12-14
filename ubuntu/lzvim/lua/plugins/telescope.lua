24b4bc111ac8fb48e1f0d074f150c9260dc44446
tdhuan <dinhhuantran3010@gmail.com>
2024-12-15 01:04:46 +0700

    [ubuntu] - Add `flex` layout strategy for Telescope with dynamic mirroring

--- ubuntu/lzvim/lua/plugins/telescope.lua
+++ ubuntu/lzvim/lua/plugins/telescope.lua
@@ -6,11 +6,22 @@ return {
   },
   opts = {
     defaults = {
+      layout_strategy = "flex",
       layout_config = {
+        flex = {
+          flip_columns = 140, -- Switch to vertical layout when width is below 130 columns
+        },
         horizontal = {
-          preview_width = 0.45,
+          width = 0.8,
+          height = 0.8,
+          preview_width = 0.5,
+          mirror = false,
+        },
+        vertical = {
+          preview_height = 0.45,
           width = 0.9, -- 90% of the screen
-          height = 0.9, -- 90% of the screen
+          height = 0.7, -- 90% of the screen
+          mirror = true,
         },
       },
     },
