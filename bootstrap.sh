820606a84046d96964f6133e873b9303a20bb840
huantd <dinhhuantran3010@gmail.com>
2025-02-26 14:06:46 +0700

    Update bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+echo "Create workspaces directory"
+mkdir $HOME/workspaces
+echo "Create testing directory"
+mkdir $HOME/testing
+echo "Create learning directory"
+mkdir $HOME/learning
+echo "Create ~/.config/lzvim directory"
+mkdir $HOME/.config/lzvim
+echo "Create ~/.config/nvim directory"
+mkdir $HOME/.config/nvim
+echo "Create ~/.config/kitty directory"
+mkdir $HOME/.config/kitty
 echo "Create screenshots directory"
 mkdir $HOME/screenshots
+echo "Install bat"
 brew install bat
+echo "Install ripgrep"
+brew install ripgrep
+echo "Install stow"
+brew install stow
