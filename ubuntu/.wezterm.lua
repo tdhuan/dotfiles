5ea701dbd84b848beffc2026910191d1beedc924
tdhuan <dinhhuantran3010@gmail.com>
2025-09-18 17:48:30 +0700

    ubuntu: add wezterm configuration

---
+++ ubuntu/.wezterm.lua
@@ -0,0 +1,36 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+-- Color scheme
+config.color_scheme = "Atlas (base16)"
+
+-- Font configuration for Ubuntu
+config.font = wezterm.font("Iosevka Term", { weight = "DemiBold" })
+config.font_size = 11.0
+
+-- Window configuration
+config.window_padding = {
+	left = 10,
+	right = 10,
+	top = 10,
+	bottom = 10,
+}
+
+-- Tab bar
+config.use_fancy_tab_bar = false
+config.tab_max_width = 100
+
+-- Disable default key bindings that conflict with vim
+config.keys = {
+	{
+		key = "Enter",
+		mods = "ALT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
+
+-- Scrollback
+config.scrollback_lines = 10000
+
+return config
